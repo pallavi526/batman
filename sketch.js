5496540aaa12dc4f228aa8dc9574c04c2b92c18a
surajv3 <surajv3@yahoo.com>
2021-02-08 19:28:04 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=100;
+
+
+
+function preload(){
+    
+}
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(1200,600);
+    
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,400), random(0,400)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    background(0); 
+
+    
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].createDrop;
+        drops[i].updateY;
+        
+    }
+
+    drawSprites();
+}  
+
